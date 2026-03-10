2002d2232e03b262320f92dff516846a47e0e624
Eduardo Pjradko <eduardoabp@gmail.com>
2026-03-10 19:21:32 +0000

    feat: add container for functions artifacts

--- app/infrastructure/main/function.tf
+++ app/infrastructure/main/function.tf
@@ -1,3 +1,11 @@
+resource "azurerm_storage_account" "dataproject_function_st" {
+  name                     = "st${var.application_name}${var.environment_name}datagen"
+  resource_group_name      = azurerm_resource_group.dataproject_rg.name
+  location                 = azurerm_resource_group.dataproject_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
 resource "azurerm_service_plan" "data_gen_asp" {
   name                = "asp-${var.application_name}-${var.environment_name}"
   resource_group_name = azurerm_resource_group.dataproject_rg.name
@@ -11,8 +19,8 @@ resource "azurerm_linux_function_app" "data_gen_func" {
   resource_group_name = azurerm_resource_group.dataproject_rg.name
   location            = azurerm_resource_group.dataproject_rg.location
 
-  storage_account_name       = azurerm_storage_account.dataproject_st.name
-  storage_account_access_key = azurerm_storage_account.dataproject_st.primary_access_key
+  storage_account_name       = azurerm_storage_account.dataproject_function_st.name
+  storage_account_access_key = azurerm_storage_account.dataproject_function_st.primary_access_key
   service_plan_id            = azurerm_service_plan.data_gen_asp.id
 
   site_config {
